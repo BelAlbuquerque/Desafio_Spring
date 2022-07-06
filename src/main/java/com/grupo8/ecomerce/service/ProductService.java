8a3b60a3f6ef3199839bf9e29ddb5196e15c2cca
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-06 14:56:37 -0400

    implementa metodo getAllProducts no Service

--- src/main/java/com/grupo8/ecomerce/service/ProductService.java
+++ src/main/java/com/grupo8/ecomerce/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.grupo8.ecomerce.service;
 
+import com.grupo8.ecomerce.dto.ProductDto;
 import com.grupo8.ecomerce.model.Product;
+
+import java.util.List;
 
 public interface ProductService {
 
     void createProduct(Product newProduct);
-
-
+    List<ProductDto> getAllProducts();
 }
