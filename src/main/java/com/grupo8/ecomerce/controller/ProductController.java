69393f0c83ca1f810ed16ffdb7ac70ed19ff0140
sandiouriquemeli <sandi.ourique@mercadolivre.com>
2022-07-06 15:11:32 -0300

    implementacao do metodo create no controller e teste insercao ok

--- src/main/java/com/grupo8/ecomerce/controller/ProductController.java
+++ src/main/java/com/grupo8/ecomerce/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.grupo8.ecomerce.controller;
 
+import com.grupo8.ecomerce.model.Product;
+import com.grupo8.ecomerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProductController {
+
+    @Autowired
+    private ProductService product;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody Product newProduct){
+        product.createProduct(newProduct);
+    }
+
 }
