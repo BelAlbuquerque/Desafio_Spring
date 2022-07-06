39e60c14580fe8729c9468d7629918e3957ff13d
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-06 11:05:49 -0300

    initial commit

---
+++ src/main/java/com/grupo8/ecomerce/EcomerceApplication.java
@@ -0,0 +1,13 @@
+package com.grupo8.ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo8/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo8.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
