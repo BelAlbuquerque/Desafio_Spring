0fd58e006a4ad610bedce6bd31faf6fc07da0468
sandiouriquemeli <sandi.ourique@mercadolivre.com>
2022-07-06 14:48:25 -0300

    implementando o metodo create no service

--- src/main/java/com/grupo8/ecomerce/service/ProductServiceImp.java
+++ src/main/java/com/grupo8/ecomerce/service/ProductServiceImp.java
@@ -1,4 +1,18 @@
 package com.grupo8.ecomerce.service;
 
-public class ProductServiceImp {
+import com.grupo8.ecomerce.model.Product;
+import com.grupo8.ecomerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImp implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public void createProduct(Product newProduct) {
+        productRepository.createProduct(newProduct);
+    }
 }
