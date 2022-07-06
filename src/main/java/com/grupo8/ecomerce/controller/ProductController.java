a936f5773a132a8dbd7cc173214dee0213e0d168
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-06 15:05:57 -0400

    implementa getAllProducts no Controller

--- src/main/java/com/grupo8/ecomerce/controller/ProductController.java
+++ src/main/java/com/grupo8/ecomerce/controller/ProductController.java
@@ -1,24 +1,31 @@
 package com.grupo8.ecomerce.controller;
 
+import com.grupo8.ecomerce.dto.ProductDto;
 import com.grupo8.ecomerce.model.Product;
 import com.grupo8.ecomerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
 
     @Autowired
-    private ProductService product;
+    private ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody Product newProduct){
-        product.createProduct(newProduct);
+        productService.createProduct(newProduct);
     }
 
-
-
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        List<ProductDto> productDtoList = productService.getAllProducts();
+        return ResponseEntity.ok(productDtoList);
+    }
 }
