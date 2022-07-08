5113910b32ec0f44bf834219f058cd0b621e79ce
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-08 19:56:41 -0300

    feat: implementa exceptions ClientAlreadyExists e IncorrectFields

--- src/main/java/com/grupo8/ecomerce/exceptions/ClientAlreadyExists.java
+++ src/main/java/com/grupo8/ecomerce/exceptions/ClientAlreadyExists.java
@@ -1,2 +1,11 @@
-package com.grupo8.ecomerce.exceptions;public class ClientAlreadyExists {
+package com.grupo8.ecomerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class ClientAlreadyExists extends RuntimeException {
+    public ClientAlreadyExists(String mensagem) {
+        super(mensagem);
+    }
 }
