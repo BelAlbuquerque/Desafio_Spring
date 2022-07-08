132e53a35e584ba8a65ad646a5d7a7c3c48c5ea6
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-08 18:34:32 -0300

    implementa classe que 'administra' erros

--- src/main/java/com/grupo8/ecomerce/handlerExceptions/HandlerExceptions.java
+++ src/main/java/com/grupo8/ecomerce/handlerExceptions/HandlerExceptions.java
@@ -1,2 +1,38 @@
-package com.grupo8.ecomerce.handlerExceptions;public class HandlerExceptions {
+package com.grupo8.ecomerce.handlerExceptions;
+
+import com.grupo8.ecomerce.exceptions.ErrorResponseDetails;
+import com.grupo8.ecomerce.exceptions.NotFoundProducts;
+import com.grupo8.ecomerce.exceptions.ServerError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class HandlerExceptions {
+    @ExceptionHandler(NotFoundProducts.class)
+    public ResponseEntity<ErrorResponseDetails> handlerError(NotFoundProducts notFoundProducts) {
+        return new ResponseEntity<>(
+                ErrorResponseDetails.builder()
+                        .title("Not Found Products")
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .message(notFoundProducts.getMessage())
+                        .timestamp(LocalDateTime.now())
+                        .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ServerError.class)
+    public ResponseEntity<ErrorResponseDetails> handlerError(ServerError serverError) {
+        return new ResponseEntity<>(
+                ErrorResponseDetails.builder()
+                        .title("Internal Server Error")
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .message(serverError.getMessage())
+                        .timestamp(LocalDateTime.now())
+                        .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
