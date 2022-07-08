65bddce56c2ecb4e634d11c485f23842acd53f0b
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-07-08 18:33:16 -0300

    feat: implementa classe de exception para ServerError

--- src/main/java/com/grupo8/ecomerce/exceptions/ServerError.java
+++ src/main/java/com/grupo8/ecomerce/exceptions/ServerError.java
@@ -1,2 +1,12 @@
-package com.grupo8.ecomerce.exceptions;public class ServerError {
+package com.grupo8.ecomerce.exceptions;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class ServerError extends RuntimeException {
+    public ServerError(String mensagem) {
+        super(mensagem);
+    }
 }
